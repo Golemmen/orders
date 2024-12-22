5b9498951171dcc8feff1e93aba5da227097d9ec
golden <tolegenmurat6@gmail.com>
2024-12-22 16:40:15 +0500

    депресия

--- src/main/java/com/example/orders/model/Order.java
+++ src/main/java/com/example/orders/model/Order.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Order {
     private Long id;
-    private List<Item> items;
+    private @NotEmpty List<String> items;
     private Double totalPrice;
     private LocalDateTime orderDate;
 
